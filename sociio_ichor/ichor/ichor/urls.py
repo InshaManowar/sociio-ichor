53b8dd5a837cf55bb63f6bd2ed57ee96f283625d
inshamanowar <inshamanowar22@gmail.com>
2021-08-12 13:08:49 +0530

    new

--- sociio_ichor/ichor/ichor/urls.py
+++ sociio_ichor/ichor/ichor/urls.py
@@ -20,10 +20,9 @@ urlpatterns = [
     path('', include('team.urls'))
     
 ]
-
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
     
-handler404 = home.views.error_404
-handler500 = home.views.error_500
+#handler404 = home.views.error_404
+#handler500 = home.views.error_500
