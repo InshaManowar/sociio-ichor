fece898f421988f57d41a628b378315a38a65ada
inshamanowar <inshamanowar22@gmail.com>
2021-08-25 12:11:19 +0530

    edits 25/08

--- sociio_ichor/ichor/home/forms.py
+++ sociio_ichor/ichor/home/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.forms import widgets
 from django.forms.models import ModelForm
 from home.models import DonorRequest
+from django.forms.fields import DateField
+from django.contrib.admin.widgets import AdminDateWidget
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length = 150)
@@ -16,14 +18,16 @@ class DonorForm(forms.ModelForm):
     
     class Meta:
         model = DonorRequest
-        fields = ['full_name', 'email_id', 'date_of_birth', 'phone', 'city', 'state','pin_code', 'weight', 'blood_group','agree']
+        fields = ['full_name', 'email_id', 'date_of_birth', 'phone', 'city', 'state','pin_code', 'weight', 'blood_group','I_agree_to_the_terms_and_conditions_stated_below',]
         widgets = {
+            
+            
             'full_name': forms.TextInput(attrs = {'class':'form-control'}),
             'city': forms.TextInput(attrs = {'class':'form-control'}),
             'state': forms.TextInput(attrs = {'class':'form-control'}),
             'pin_code': forms.NumberInput(attrs = {'class':'form-control'}),
             'phone': forms.TextInput(attrs = {'class':'form-control'}),
-            'date_of_birth': forms.TextInput(attrs = {'label':'Date of birth', 'class':'form-control'}),
-            'weight': forms.NumberInput(attrs = {'class':'form-control'}),
+            'date_of_birth': forms.DateInput(attrs={'placeholder': 'YYYY-MM-DD', 'class':'form-control'}),
+            'weight': forms.NumberInput(attrs = {'class':'form-control', 'placeholder':'Body weight'}),
             'email_id': forms.EmailInput(attrs = {'class':'form-control'}),
         }
--- sociio_ichor/ichor/home/models.py
+++ sociio_ichor/ichor/home/models.py
@@ -66,14 +66,14 @@ pre_save.connect(pre_save_blog_post_receiver, sender=BloodRequest)
 
 class DonorRequest(models.Model):
     full_name = models.CharField(max_length=200, default='')
-    date_of_birth = models.CharField(max_length=11, default='')
+    date_of_birth = models.DateField(default='')
     email_id = models.EmailField(max_length=200)
     phone = models.CharField(max_length=11, default='')
     city = models.CharField(max_length=100, default='')
     state = models.CharField(max_length=100, default='')
     pin_code = models.IntegerField(default=False)
     weight = models.CharField(max_length=10, default='')
-    agree = models.BooleanField(blank=True,  default=False)
+    I_agree_to_the_terms_and_conditions_stated_below = models.BooleanField(blank=False,  default=True)
     blood_group = models.CharField(
         max_length=4, choices=BG_CHOICES, default=None
     )    
